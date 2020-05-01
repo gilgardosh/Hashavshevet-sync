81ff7570ba77c6f18da52dab08efa577e51fe0f9
gilgardosh <gilgardosh@gmail.com>
2020-05-01 16:10:53 +0300

    working with live bank and hash data

--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
 import { getHashData } from "./utils/wizcloudProccess";
 import { getBankData } from "./utils/bankProccess";
 import { compareBankHashData } from "./utils/compareAlgorithm";
+import { date } from "./utils/getDate";
 
-let hashData: object[];
-const bankData: object[] = (getBankData("./demiData/demiBankData.csv"));
+async function executeSync(months) {
+  const promises = [
+    getBankData(date.startDateISO(months)),
+    getHashData(date.startDateHash(months)),
+  ];
+  try {
+    const results = await Promise.all(promises);
+    let bankData = results[0];
+    let hashData = results[1];
+    
+    let comparisonJson: object = compareBankHashData(bankData, hashData);
+    // TODO: function that creates log file of comparison
+    console.log("comparisonJson", comparisonJson);
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+}
 
-getHashData().then((res) => {
-  hashData = res;
-  // console.log(hashData);
-  console.log(compareBankHashData(bankData, hashData));
-});
+executeSync(6);
--- utils/bankProccess.ts
+++ utils/bankProccess.ts
@@ -1,18 +1,74 @@
 import * as fs from "fs";
+import { config } from "dotenv";
 
-const keyDict = {
+config();
+
+const bankUserCode = process.env.BANK_USER_CODE;
+const bankPassword = process.env.BANK_PASSWORD;
+let accountsData = [];
+
+const bankCsvDict = {
   "קוד תנועה": "transID",
-  "תאריך": "valueDate",
+  תאריך: "valueDate",
   "תיאור הפעולה": "Description",
-  "חשבון": "Account",
-  "אסמכתא": "Reference",
+  חשבון: "Account",
+  אסמכתא: "Reference",
   "תאריך ערך": "DueDate",
-  "חובה": "DebVal",
-  "זכות": "CredVal",
-  "יתרה לאחר פעולה": "PostBalance"
+  חובה: "DebVal",
+  זכות: "CredVal",
+  "יתרה לאחר פעולה": "PostBalance",
+};
+
+const bankApiDict = {
+  "type": "TransType",
+  "identifier": "transID",
+  "date": "DueDate",
+  "processedDate": "valueDate",
+  "originalAmount": "OriginalSuF",
+  "originalCurrency": "CurrencyCode",
+  "chargedAmount": "SuF",
+  "description": "Details",
+  "status": 'completed',
+  "memo": "Reference"
 }
 
-function getBankData(csvLink) {
+async function getBankData(startDate) {
+  const { createScraper } = require("israeli-bank-scrapers");
+
+  const credentials = {
+    userCode: bankUserCode,
+    password: bankPassword,
+  };
+  const options = {
+    companyId: "hapoalim",
+    startDate: startDate,
+    combineInstallments: false, // if set to true, all installment transactions will be combine into the first one
+    showBrowser: true, // shows the browser while scraping, good for debugging (default false)
+    verbose: false, // include more debug info about in the output
+  };
+
+  return await (async function () {
+    try {
+      const scraper = createScraper(options);
+      const scrapeResult = await scraper.scrape(credentials);
+
+      if (scrapeResult.success) {
+        scrapeResult.accounts.forEach((account) => {
+          // console.log(`found ${account.txns.length} transactions for account number
+          //   ${account.accountNumber}`);
+          accountsData.push(keysToHashFormat(account.txns, bankApiDict));
+        });
+        return accountsData;
+      } else {
+        throw new Error(scrapeResult.errorType);
+      }
+    } catch (e) {
+      console.error(`scraping failed for the following reason: ${e.message}`);
+    }
+  })();
+  /* code for local file proccessing (function should get file address as csvLink)
+  ////////////////////////////////////////////////////////////////////////////////
+  const csvLink = process.env.CSV_LINK;
   const lines = fs.readFileSync(csvLink).toString().split("\n");
   const result = [];
   const headers = lines[0].split(",");
@@ -27,10 +83,12 @@ function getBankData(csvLink) {
     }
     result.push(obj);
   }
-  return keysToLatin(result);
+  return [keysToHashFormat(result, bankCsvDict)];
+  //////////////////////////////////////////////////////////////////////////////*/
 }
 
-function keysToLatin(data: object[]) {
+function keysToHashFormat(data: object[], keyDict) {
+  
   function arrayKeysToLatin(allTransData: object[]) {
     for (let i = 0; i < allTransData.length; i++) {
       allTransData[i] = objectKeysToLatin(allTransData[i]);
@@ -43,12 +101,13 @@ function keysToLatin(data: object[]) {
     for (const key in singleTransData)
       if (singleTransData.hasOwnProperty(key)) {
         const trimKey = key.trim();
-        if (trimKey === "" && singleTransData[key].trim() === "") {
+        if (trimKey === "") {
           continue;
         }
         if (singleTransData[key] != null) {
           if (keyDict[trimKey]) {
-            newData[keyDict[trimKey]] = singleTransData[key].trim();
+            if (typeof(keyDict[trimKey])=="string") {keyDict[trimKey].trim()};
+            newData[keyDict[trimKey]] = singleTransData[key];
           } else {
             console.log("ERROR: missing key in dictionary", trimKey);
 
@@ -58,7 +117,6 @@ function keysToLatin(data: object[]) {
       }
     return newData;
   }
-
   return arrayKeysToLatin(data);
 }
 
