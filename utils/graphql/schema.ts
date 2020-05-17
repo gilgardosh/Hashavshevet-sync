e2a1e3ac669b60655a6fbf13d59225195af2fd35
gilgardosh <gilgardosh@gmail.com>
2020-05-17 22:14:37 +0300

    TransactionType created

---
+++ utils/graphql/schema.ts
@@ -0,0 +1,56 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  fields: () => ({
+    // book: {
+    //   type: BookType,
+    //   description: "A Single Book",
+    //   args: {
+    //     id: { type: GraphQLInt },
+    //   },
+    //   resolve: (_, args) => books.find((book) => book.id === args.id),
+    // },
+    // books: {
+    //   type: new GraphQLList(BookType),
+    //   description: "List of All Books",
+    //   resolve: () => books,
+    // },
+    // author: {
+    //   type: AuthorType,
+    //   description: "A Single Author",
+    //   args: {
+    //     id: { type: GraphQLInt },
+    //   },
+    //   resolve: (_, args) => authors.find((author) => author.id === args.id),
+    // },
+    // authors: {
+    //   type: new GraphQLList(AuthorType),
+    //   description: "List of All Authors",
+    //   resolve: () => authors,
+    // },
+    transactionById: {
+        type: TransactionType,
+        description: "A Single Transaction",
+        args: {
+            id: { type: GraphQLInt },
+        },
+        resole: (_, args) => transactions.find((trans) => trans.id === args.id),
+    },
+    
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: "RootMutationType", //add real mutation
+});
+
+export { schema };
