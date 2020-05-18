738f19953d1be29dee65cdba1dc89617023ceb05
gilgardosh <gilgardosh@gmail.com>
2020-05-18 09:11:45 +0300

    basic schema and transaction type added

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import * as fs from "fs";
 import { getHashData } from "./utils/wizcloudProccess/wizcloudProccess";
 import { getAccountsList } from "./utils/wizcloudProccess/getAccountsList";
-import { getBankDemiData,getBankData } from "./utils/bankProccess/bankProccess";
+import {
+  getBankDemiData,
+  getBankData,
+} from "./utils/bankProccess/bankProccess";
 import { compareBankHashData } from "./utils/compareAlgorithm";
 import { date } from "./utils/getDate";
 
@@ -18,11 +21,13 @@ async function executeSync(months) {
     const accountsList = results[2];
     saveToFile("./indexes/accountsList.json", accountsList);
     console.log("accountsList.json Index updated in indexes lib");
-    
 
     const comparisonJson: object = compareBankHashData(bankData, hashData);
     // TODO: function that creates log file of comparison
-    saveToFile(`./logs/${new Date().getTime()}_comparison.json`, comparisonJson);
+    saveToFile(
+      `./logs/${new Date().getTime()}_comparison.json`,
+      comparisonJson
+    );
     console.log("Comparison log created in logs lib");
   } catch (error) {
     console.log("ERROR:", error);
@@ -35,9 +40,26 @@ function saveToFile(filename: string, jsonObj) {
   fs.writeFile(`${filename}`, jsonContent, "utf8", function (err) {
     if (err) {
       console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
-    };
+      console.log(err);
+    }
   });
 }
 
 executeSync(2);
+
+/////////////////////////////////////////////////////////////////////////////////
+
+import express from "express";
+import expressGraphQL from "express-graphql";
+import { schema } from "./utils/graphql/schema";
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+    schema: schema,
+  })
+);
+app.listen(5000, () => console.log("Server Running"));
