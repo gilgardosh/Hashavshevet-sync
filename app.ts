1aca8c26eb9deaeef7ed1838cee2c2a84448301b
gilgardosh <gilgardosh@gmail.com>
2020-07-08 14:16:25 +0300

    added swagger editor

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import expressGraphQL from "express-graphql";
 import { schema, createSDL } from "./src/schema";
-import { useSofa } from 'sofa-api';
+import { useSofa, OpenAPI } from 'sofa-api';
 import bodyParser from "body-parser";
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 
 const app = express();
 const PORT: number = 5000;
+
+const openApi = OpenAPI({
+  schema,
+  info: {
+    title: 'Example API',
+    version: '3.0.0',
+  },
+});
 
 
 app.use(bodyParser.json());
@@ -18,7 +28,19 @@ app.use(
   })
 );
 
-app.use('/api', useSofa({ schema }));
+app.use('/api', useSofa({
+  schema,
+  onRoute(info) {
+    openApi.addRoute(info, {
+      basePath: '/api',
+    });
+  },
+}));
+
+openApi.save('./swagger.json');
+openApi.save('./swagger.yml');
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 createSDL();
 
