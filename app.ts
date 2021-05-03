a21f4d5121fed5fc6e9aebe6cb0a7bf38b9687a0
gilgardosh <gilgardosh@gmail.com>
2021-05-03 18:27:51 +0300

    linting

--- app.ts
+++ app.ts
@@ -1,41 +1,43 @@
-import express from "express";
-import expressGraphQL from "express-graphql";
-import { schema, createSDL } from "./src/schema";
+import express from 'express';
+import expressGraphQL from 'express-graphql';
+import { schema, createSDL } from './src/schema';
 import { useSofa, OpenAPI } from 'sofa-api';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger.json';
 
 const app = express();
-const PORT: number = 5000;
+const PORT = 5000;
 
 const openApi = OpenAPI({
-  schema,
-  info: {
-    title: 'Example API',
-    version: '3.0.0',
-  },
+    schema,
+    info: {
+        title: 'Example API',
+        version: '3.0.0',
+    },
 });
-
 
 app.use(bodyParser.json());
 
 app.use(
-  "/graphql",
-  expressGraphQL({
-    graphiql: true,
-    schema,
-  })
+    '/graphql',
+    expressGraphQL({
+        graphiql: true,
+        schema,
+    }),
 );
 
-app.use('/api', useSofa({
-  schema,
-  onRoute(info) {
-    openApi.addRoute(info, {
-      basePath: '/api',
-    });
-  },
-}));
+app.use(
+    '/api',
+    useSofa({
+        schema,
+        onRoute(info) {
+            openApi.addRoute(info, {
+                basePath: '/api',
+            });
+        },
+    }),
+);
 
 openApi.save('./swagger.json');
 openApi.save('./swagger.yml');
@@ -45,5 +47,5 @@ app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 createSDL();
 
 app.listen(PORT, () => {
-  console.log(`GrapiQL Server Running on http://localhost:${PORT}/graphql`);
+    console.log(`GrapiQL Server Running on http://localhost:${PORT}/graphql`);
 });
